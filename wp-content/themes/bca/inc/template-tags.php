79cbd810ec8a2b95cc6e084143dab83d54cd0e80
rememberlenny <lkbgift@gmail.com>
2013-09-19 16:20:52 -0700

    Suttle

--- wp-content/themes/bca/inc/template-tags.php
+++ wp-content/themes/bca/inc/template-tags.php
@@ -177,16 +177,11 @@ function bullettshop_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	printf( __( '<span class="posted-on">Posted on %1$s</span><span class="byline"> by %2$s</span>', 'bullettshop' ),
+	printf( __( '<span class="posted-on">Posted on %1$s</span>', 'bullettshop' ),
 		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a>',
 			esc_url( get_permalink() ),
 			esc_attr( get_the_time() ),
 			$time_string
-		),
-		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_attr( sprintf( __( 'View all posts by %s', 'bullettshop' ), get_the_author() ) ),
-			esc_html( get_the_author() )
 		)
 	);
 }
